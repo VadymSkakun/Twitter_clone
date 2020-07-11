418019d0d299912f30d803a28706bb2c6d48aaef
VadymSkakun <jamal_q@ukr.net>
2020-07-11 21:31:01 +0300

    clone

---
+++ src/main/java/com/jamal/twitter/repos/MessageRepo.java
@@ -0,0 +1,10 @@
+package com.jamal.twitter.repos;
+
+import com.jamal.twitter.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
+}
